51dc767e358d7e1773a71bddadf283aeaf1c882f
kateikyoshi <kateikyoshi.job@gmail.com>
2024-08-23 21:26:39 +0300

    feat: initial commit

---
+++ src/main/java/jp/warau/bakari/BakariApplication.java
@@ -0,0 +1,13 @@
+package jp.warau.bakari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/warau/bakari/BakariApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.warau.bakari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
